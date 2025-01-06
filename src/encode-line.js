0c63cf213eedc910e6541e0396b56083e1fad38a
Vl Lena <tg @vlpro>
2025-01-07 01:12:07 +0300

    feat: add solution for encode line

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,7 +10,31 @@ const { NotImplementedError } = require("../extensions/index.js");
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(str) {}
+function encodeLine(str) {
+  let result = "";
+  let currentChar = "";
+  let count = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+
+    if (char === currentChar) {
+      count++;
+    } else {
+      if (count > 0) {
+        result += (count > 1 ? count : "") + currentChar;
+      }
+      currentChar = char;
+      count = 1;
+    }
+  }
+
+  if (count > 0) {
+    result += (count > 1 ? count : "") + currentChar;
+  }
+
+  return result;
+}
 
 module.exports = {
   encodeLine,
